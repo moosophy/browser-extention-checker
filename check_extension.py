44a74c5e290fdab51b132a72a210a9bce9ace95a
Katsiaryna Yakubava <yekaterinayak@gmail.com>
2025-06-23 11:26:47 +0200

    more fingerprinting check

--- check_extension.py
+++ check_extension.py
@@ -6,11 +6,13 @@ found_fingerprint = set()
 
 # flags for malicious activities
 flags = {
+    "fingerprint_low": False,
+    "fingerprint_medium": False,
+    "fingerprint_high": False,
     "file_reading": False,
-    "fingerprint": False,
     "fileSystemProvider_use": False,
     "gcm_use": False,
-    "cookie_use": False,
+    "cookies_use": False,
     "clipboard_use": False,
     "settings_use": False,
     "debugger_use": False,
@@ -35,7 +37,6 @@ flags = {
 }
 
 
-
 #Recursively parse a directory, to read the contents of all the files in that directory
 # and all the files in it's subdirectory
 def scanFolder(directory):
@@ -54,44 +55,18 @@ def scanFolder(directory):
 def checkFile(path, filename):
     file_path = os.path.join(path, filename)
     with open(file_path, errors="ignore") as f:
-        #--------------------READING THE CONTENT HERE---------------------------
-        #checks if it reads submitted files
         content = f.read()
-        found_keywords = [keyword for keyword in file_access_keywords if keyword in content]
-        if found_keywords:
-            flags["file_reading"] = True
-            # print(f"{found_keywords} found in the file {file_path}!\n")
-
-
-        #Cheking for fingerprinting
-        count = 0
-        for pattern in fingerprint_patterns:
-            if re.search(pattern, content):
-                count+=1
-                found_fingerprint.add(pattern)
-        if count > 3:
-            flags["fingerprint"] = True
-            
-
         #-------------------------- PERMISSIONS --------------------------------
         if filename == "manifest.json":
             if "fileSystemProvider" in content:
                 flags["fileSystemProvider_use"] = True
-                # print(f"This file uses fileSystemProvider: {file_path}")
-            if "gcm" in content:
-                flags["gcm_use"] = True
-                # print(f"This file uses GCM: {file_path}")
-            if "cookies" in content:
-                flags["cookie_use"] = True
-                found_fingerprint.add("cookies")
-                # print(f"This file uses cookies: {file_path}")
-
+            if "gcm" in content: flags["gcm_use"] = True
+            if "cookies" in content: flags["cookies_use"] = True
             
             #Now for permissions that chrome commonly warns about:
             if "clipboardRead" in content: flags["clipboard_use"] = True
             if "contentSettings" in content: flags["settings_use"] = True
             if "debugger" in content: flags["debugger_use"] = True
-            if "clipboardRead" in content: flags["clipboard_use"] = True
             if "declarativeNetRequest" in content: flags["declarativeNetRequest_use"] = True
             if "declarativeNetRequestFeedback" in content: flags["declarativeNetRequestFeedback_use"] = True
             if "desktopCapture" in content: flags["desktopCapture_use"] = True
@@ -110,17 +85,44 @@ def checkFile(path, filename):
             if "tabs" in content: flags["tabs_use"] = True
             if "webAuthenticationProxy" in content: flags["webAuthenticationProxy_use"] = True
             if "webNavigation" in content: flags["webNavigation_use"] = True
-            
-            
+
+        #--------------------READING SUSPICIOUS FILE CONTENT---------------------------
+        found_keywords = [keyword for keyword in file_access_keywords if keyword in content]
+        if found_keywords:
+            flags["file_reading"] = True
+            # print(f"{found_keywords} found in the file {file_path}!\n")
+
+        #Cheking for fingerprinting
+        for pattern in fingerprint_patterns:
+            if re.search(pattern, content):
+                found_fingerprint.add(pattern)
+
+
+
+def evaluateMalicious():
+    for permission in fingerprint_permissions:
+        if flags[permission]:
+            found_fingerprint.add(permission) 
+
+    if len(found_fingerprint)>8:
+        flags["fingerprint_high"] = True
+    elif len(found_fingerprint)>5:
+        flags["fingerprint_medium"] = True
+    elif len(found_fingerprint)>2:
+        flags["fingerprint_low"] = True
         
+
 
 def reportMalicious():
     flag_messages = {
+    "fingerprint_low": f"‣ This extension collects some device or browser characteristics that could be used for tracking.\n\tDetails collected:\n\t{found_fingerprint}",
+    "fingerprint_medium": f"‣ This extension collects a lot of browser and device details. This data can be used for profiling.\n\tDetails collected:\n\t{found_fingerprint}",
+    "fingerprint_high": f"‣ This extension collects an extensive amount of fingerprinting data. This extension might be profiling you or selling your data.\n\tDetails collected:\n\t{found_fingerprint}",
+    
     "gcm_use": "‣ This extension can receive remote commands from an external server using Google's push messaging system (GCM). If the extension is not clearly advertized to provide real-time updates or messages, this is highly suspicious.",
     "fileSystemProvider_use": "‣ This extension deals with remote files. Use it cautiously.",
-    "fingerprint": f"‣ This extension collects a broad range of device and browser details, which may be used for tracking.\n\tDetails collected:\n\t{found_fingerprint}",
     "file_reading": "‣ This extension can read submitted files! Avoid exposing sensitive data.",
-    "cookie_use": "‣ This extension uses cookies. Be careful if paired with external communication.",
+    "cookies_use": "‣ This extension uses cookies. Be careful if paired with external communication.",
     "clipboard_use": "‣ This extension can read or modify clipboard content. If unnecessary, it could be used to steal copied information.",
     "settings_use": "‣ This extension modifies browser content settings. This could weaken security or allow unwanted behavior.",
     "debugger_use": "‣ This extension uses the debugger API, which allows full DevTools access to any tab. This is extremely dangerous unless it’s a trusted developer tool.",
@@ -154,15 +156,14 @@ def reportMalicious():
 
 def main():
     print("\nParsing files...")
-
     scanFolder(sys.argv[1])
-
     print("Parsing finished.\n")
+
+    evaluateMalicious()
+
     print("-"*50 + "\n")
     print("RESULTS:")
-
     reportMalicious()
-
     print("\n" + "-"*50)
 
 
--- patterns_and_keywords.py
+++ patterns_and_keywords.py
@@ -6,14 +6,29 @@ file_access_keywords = ["FileReader", "readAsText", "readAsDataURL", "readAsBina
 
 
 fingerprint_patterns = [
-    r"getImageData\s*\(",
-    r"toDataURL\s*\(",
+    #canvas
+    r"\.getImageData\s*\(",
+    r"\.toDataURL\s*\(",
+    #audio 
     r"AudioContext",
     r"OscillatorNode",
     r"getFloatFrequencyData",
+    #navigator
     r"navigator\.hardwareConcurrency",
     r"navigator\.deviceMemory",
     r"navigator\.plugins",
     r"navigator\.languages",
+    #screen
     r"screen\.(width|height)"
+    #time (can specify timezone)
+    r"new\s+Date\s*\("
+    r"Intl\.DateTimeFormat\s*\("
+    #the bottow ones are permissons, but only used for fingerpringing
+    r"chrome\.topSites\.get\s*\("
+    r"chrome\.system\.cpu\.getInfo\s*\("
+    r"chrome\.system\.memory\.getInfo\s*\("
+    r"chrome\.system\.display\.getInfo\s*\("
 ]
+
+fingerprint_permissions = {"processes_use", "tabs_use", "topSites_use", "history_use", 
+                           "cookies_use", "identity_use"}
